4a31e33566cc004b13a139cdaefc5957fb41d274
Balram02 <balramkukreja@yahoo.com>
2020-05-06 04:15:13 +0530

    added router module and product details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { from } from 'rxjs';
 import { ConvertToCharacter } from './shared/convert-to-character';
 import { StarComponent } from './shared/star.component';
+import { ProductDetailsComponent } from './products/product-details.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, StarComponent, ConvertToCharacter],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, ProductListComponent, StarComponent, ConvertToCharacter, ProductDetailsComponent, WelcomeComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot([
+
+    { path: 'products', component: ProductListComponent },
+    { path: 'product/:id', component: ProductDetailsComponent },
+    { path: 'welcome', component: WelcomeComponent },
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+
+  ])],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
